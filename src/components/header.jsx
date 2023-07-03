d4b77edfaf5d750b9c7a301d9896755a13e2c4bb
Calpurniax <sofia.gracia@gmail.com>
2023-07-03 20:49:10 +0200

    create header and icon component, create styles and variables/mixin/reset

---
+++ src/components/header.jsx
@@ -0,0 +1,29 @@
+import "../syles/components/header.scss"
+import sofiaPhoto from "../images/foto-sofia.jpg";
+import {Icon} from "./icon"
+
+export const Header =()=>(
+    <header className="c-header">
+        <div className="c-header__border">
+            <img className = "c-header__img" alt="Sofía Gracia" src={sofiaPhoto}></img>
+            <div className="c-header__textBox">
+                <h1 className = "c-header__title">Sofía Gracia</h1>
+                <p className = "c-header__text">Front-end developer from Madrid (Spain)</p>
+                <ul className = "c-header__iconList">
+                    <li>
+                        <Icon link="https://github.com/Calpurniax"
+                            iClass={'fa-brands fa-github-alt'}/>
+                    </li>
+                    <li>
+                        <Icon link="https://twitter.com/calpurniax_"
+                        iClass={'fa-brands fa-twitter'}/>
+                    </li>
+                    <li>
+                        <Icon link="https://www.linkedin.com/in/sofia-gracia-pe%C3%B1a/"
+                        iClass={'fa-brands fa-linkedin-in'}/>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+);
---
+++ src/components/icon.jsx
@@ -0,0 +1,5 @@
+export const Icon =(props)=>(
+    <a href={props.link} target="_blank" rel="noreferrer">
+            <i className={`${props.iClass} c-header__iconList__icon ${props.dot}`}></i>
+    </a>
+)
