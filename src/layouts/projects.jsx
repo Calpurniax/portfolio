5dd152f6ca729eb8aacbf31df123e051df7c3279
Calpurniax <sofia.gracia@gmail.com>
2023-07-07 12:19:49 +0200

    add more projects, need to review css

--- src/layouts/projects.jsx
+++ src/layouts/projects.jsx
@@ -1,17 +1,36 @@
 import "../styles/layouts/projects.scss";
 import { ProjectsArticle } from "../components/projects-article";
-import rickandmorty_landing from "../images/R&M/portadaR&M_mobile.png"
+import rickandmorty_landing from "../images/r&m/portadaR&M_mobile.png"
+import taskList_landing from "../images/task_list/portadaTaskList_mobile.png"
+import casaCanto_landing from "../images/casaCanto/portadaCasaCanto_mobile.png"
+
 const Projects =()=>{
    return (
     <section className="projects">
       <h3 className="projects__title">Muestras de proyectos</h3>
-      <ProjectsArticle
-      tech1={'React'}
-      tech2={'Fetch'}
-      img={rickandmorty_landing}
-      alt={'Buscador de personajes de Rick and Morty'}
-      link={"https://calpurniax.github.io/modulo-3-evaluacion-final-Calpurniax/"}
-      title={'Buscador de personajes de Rick and Morty'}/>
+      <div className="projects__container">
+        <ProjectsArticle
+        tech1={'React'}
+        tech2={'Fetch'}
+        img={rickandmorty_landing}
+        alt={'Buscador de personajes de Rick and Morty'}
+        link={"https://calpurniax.github.io/modulo-3-evaluacion-final-Calpurniax/"}
+        title={'Buscador de personajes de Rick and Morty'}/>
+        <ProjectsArticle
+        tech1={'Wordpress'}
+        img={casaCanto_landing}
+        alt={'web Casa Canto'}
+        link={"https://casa-canto.com/"}
+        title={'Casa Canto - alquiler vacacional'}/>
+         <ProjectsArticle
+        tech1={'React'}
+        tech2={'Local Storage'}
+        img={taskList_landing}
+        alt={'Lista de tareas'}
+        link={"https://calpurniax.github.io/lista-de-tareas/"}
+        title={'Lista de tareas con local storage'}/>
+      </div>
+      
     </section>
    )
 }
