1ae0bf36c4d1b4103cb0cb8093a7cba4bef563ef
Calpurniax <sofia.gracia@gmail.com>
2023-07-08 09:18:07 +0200

    add tech button render logic

--- src/components/projects-article.jsx
+++ src/components/projects-article.jsx
@@ -1,16 +1,23 @@
 import "../styles/components/projects-article.scss"
 import { ProjectsTechButton } from "./projects-techButton"
 
-export const ProjectsArticle = (props) => (
+export const ProjectsArticle = (props) => {
+    const renderTechs=()=>{
+        if (props.tech.length>0){
+            return props.tech.map((eachTech, index)=>{        
+                return <ProjectsTechButton text={eachTech} key={index}/>
+               })
+        }       
+    }
+    return(
     <article className="projects__article">
         <div className="projects__article__container">            
-            <div className="projects__article__techDiv">
-                <ProjectsTechButton text={props.tech1}/>
-                <ProjectsTechButton text={props.tech2}/>
+            <div className="projects__article__techDiv">              
+                {renderTechs()}
             </div>
             <img src={props.img} alt={props.alt} className="projects__article__img"/>
         </div>
         <a href={props.link} className="projects__article__title">{props.title}</a>
         <div className="projects__article__borderBottom"></div>
     </article>
-)
+)};
--- src/layouts/projects.jsx
+++ src/layouts/projects.jsx
@@ -10,21 +10,20 @@ const Projects =()=>{
       <h3 className="projects__title">Muestras de proyectos</h3>
       <div className="projects__container">
         <ProjectsArticle
-        tech1={'React'}
+        tech={['React','Fetch']}
         tech2={'Fetch'}
         img={rickandmorty_landing}
         alt={'Buscador de personajes de Rick and Morty'}
         link={"https://calpurniax.github.io/modulo-3-evaluacion-final-Calpurniax/"}
         title={'Buscador de personajes de Rick and Morty'}/>
         <ProjectsArticle
-        tech1={'Wordpress'}
+        tech={['Wordpress']}
         img={casaCanto_landing}
         alt={'web Casa Canto'}
         link={"https://casa-canto.com/"}
         title={'Casa Canto - alquiler vacacional'}/>
          <ProjectsArticle
-        tech1={'React'}
-        tech2={'Local Storage'}
+        tech={['React', 'Local Storage']}        
         img={taskList_landing}
         alt={'Lista de tareas'}
         link={"https://calpurniax.github.io/lista-de-tareas/"}
