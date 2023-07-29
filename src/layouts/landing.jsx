471f6fa9bd77585624e57f45690fbbe5ac7aa70c
Calpurniax <sofia.gracia@gmail.com>
2023-07-29 16:19:33 +0200

    done css for landing in desktop version

--- src/layouts/landing.jsx
+++ src/layouts/landing.jsx
@@ -5,7 +5,9 @@ export const Landing =({pdfDocument})=>(
             <h2 className="landing__title"><span className="landing__title__span">¡Hola!</span> Te doy la bienvenida a mi portfolio</h2>
             <p className="landing__text">Soy licenciada en Comunicación Audiovisual, y he decidido abrirme un nuevo camino en el sector IT. </p>
             <p className="landing__text">Para conocer más de mi carrera profesional, puedes ir a la sección "Sobre mí" y en "Proyectos" están algunos ejemplos de mi trabajo.</p>
-            <p className="landing__text">Si quieres puedes descargar mi CV en PDF</p>
-             <Button link={pdfDocument} text="Descarga"/>
+            <div className="landing__downloadBox">
+              <p className="landing__text">Si quieres puedes descargar mi CV en PDF</p>
+               <Button link={pdfDocument} text="Descarga"/>
+            </div>
           </section>
 )
